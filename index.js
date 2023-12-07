47ec8c7a8e98cc483d0297c4610f326933e24d31
Francesco <francesco@email.com>
2023-12-07 16:46:32 +0100

    :(

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+const createCells= function (){
+    const boxDiv = document.getElementById('box')
+}
+for (let i = 1; i<77 ; i++){
+    console.log(i)}
+
+    const cellDiv = document.createElement('div')
+    // <div></div>
+cellDiv.classList.add('numbers')
